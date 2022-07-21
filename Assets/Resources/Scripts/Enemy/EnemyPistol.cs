755b05439e1de461068253d2cea917f33e06855d
MoranSD <93720897+MoranSD@users.noreply.github.com>
2022-07-21 13:58:55 +0300

    multy slice

--- Assets/Resources/Scripts/Enemy/EnemyPistol.cs
+++ Assets/Resources/Scripts/Enemy/EnemyPistol.cs
@@ -13,17 +13,8 @@ public class EnemyPistol : Enemy
         _animator.enabled = false;
         _triggerRange.enabled = false;
         GetComponent<Collider>().enabled = false;
-        //slice mesh
-        IBzSliceable sliceTarget = _sliceTarget.GetComponent<IBzSliceable>();
-        Plane slicePlane = new Plane(swordTransform.up, swordTransform.position);
-        sliceTarget.Slice(slicePlane, OnEndSlice);
-
+        
         CallOnDeathEvent();
-    }
-    void OnEndSlice(BzSliceTryResult result)
-    {
-        result.outObjectNeg.GetComponent<EnemyRagdoll>().AfterSlice();
-        result.outObjectPos.GetComponent<EnemyRagdoll>().AfterSlice();
     }
 
     protected override void OnPlayerEnterRangeZone()
