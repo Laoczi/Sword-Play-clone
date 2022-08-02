77f4118b71d95ebf8a2d11c8094f172ef318022a
MoranSD <93720897+MoranSD@users.noreply.github.com>
2022-08-02 08:33:45 +0300

    last

--- Assets/Resources/Scripts/UI/SkinsShop.cs
+++ Assets/Resources/Scripts/UI/SkinsShop.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class SkinsShop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] float _unlockRandomPrice;
+    [SerializeField] GameObject _lockedSkinsParent;
+    [SerializeField] GameObject _unlockedSkinsParent;
 
-    // Update is called once per frame
-    void Update()
+    GameObject[] _lockedSkins;
+    GameObject[] _unlockedSkins;
+
+    private void Start()
     {
-        
+        _lockedSkins = _lockedSkinsParent.GetComponentsInChildren<GameObject>();
+        _unlockedSkins = _unlockedSkinsParent.GetComponentsInChildren<GameObject>();
+
+        for (int i = 0; i < _lockedSkins.Length; i++)
+        {
+            if (PlayerPrefs.HasKey("OpenSkin " + i))
+            {
+                _lockedSkins[i].SetActive(false);
+                _unlockedSkins[i].SetActive(true);
+            }
+            else
+            {
+                _lockedSkins[i].SetActive(true);
+                _unlockedSkins[i].SetActive(false);
+            }
+        }
+    }
+    public void OpenRandom()
+    {
+        List<int> openedSkinsID = new List<int>();
+
+        for (int i = 0; i < _unlockedSkins.Length; i++)
+        {
+            if (_unlockedSkins[i].activeInHierarchy) openedSkinsID.Add(i);
+        }
+
+        if(openedSkinsID.Count == 0)
+        {
+            Debug.Log("Все скины куплены");
+            return;
+        }
+
+        if (Wallet.singleton.Get(_unlockRandomPrice) == false) return;
+
+        int randomSkin = Random.Range(0, openedSkinsID.Count);
+
+        _lockedSkins[openedSkinsID[randomSkin]].SetActive(false);
+        _unlockedSkins[openedSkinsID[randomSkin]].SetActive(true);
+        PlayerPrefs.SetInt("OpenSkin " + openedSkinsID[randomSkin], 1);
     }
 }
